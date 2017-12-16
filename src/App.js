165a1e4de4eef988ef8faa3671a656a5b743876d
Oleksandr Maksymenko <alexandr.maximenko@gmail.com>
2017-12-16 13:34:40 +0200

    test 7

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Hello!
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Hello! To get started, edit <code>src/App.js</code> and save to
+          reload.
         </p>
       </div>
     );
